2ff3ae56b851561c852ba34f29431b9a9ad66152
Danya <s02210352@gse.cs.msu.ru>
2024-02-23 23:29:57 +0300

    fixed the error

--- hw2/submission_template01.py
+++ hw2/submission_template01.py
@@ -2,13 +2,11 @@ import torch.nn as nn
 
 def create_model():
     # Linear layer mapping from 784 features, so it should be 784->256->16->10
-
     NN = nn.Sequential(nn.Linear(784, 256, bias=True),
                    nn.ReLU(),
                    nn.Linear(256, 16, bias=True),
                    nn.ReLU(),
-                   nn.Linear(16, 10, bias=True),
-                   nn.ReLU())
+                   nn.Linear(16, 10, bias=True))
 
     # return model instance (None is just a placeholder)
     return NN
