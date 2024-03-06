5cf654d3d53f6bcdc967f46c9b9652dfc20397fb
Danya <s02210352@gse.cs.msu.ru>
2024-03-07 00:41:47 +0300

    some updates

--- hw4/submission_template04.py
+++ hw4/submission_template04.py
@@ -40,39 +40,3 @@ class ConvNet(nn.Module):
 
 def create_model():
     return ConvNet()
-
-
-
-# ВАШ КОД ЗДЕСЬ
-# объявите класс сверточной нейросети
-
-# class ConvNet(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#         self.conv1 = nn.Conv2d(in_channels=3, out_channels=9, kernel_size=(3,3)) # 30
-#         self.pool1 = nn.MaxPool2d(kernel_size=(2,2)) # 15
-#         self.conv2 = nn.Conv2d(in_channels=9, out_channels=6, kernel_size=(4,4)) # 12
-#         self.pool2 = nn.MaxPool2d(kernel_size=(2,2)) # 6
-
-#         self.flatten = nn.Flatten()
-
-#         self.fc1 = nn.Linear(6 * 6 * 6, 128)
-#         self.fc2 = nn.Linear(128, 10)
-
-
-#     def forward(self, x):
-#         # размерность х ~ [64, 3, 32, 32]
-
-#         # ВАШ КОД ЗДЕСЬ
-#         # реализуйте forward pass сети
-#         x = F.relu(self.conv1(x))
-#         x = self.pool1(x)
-#         x = F.relu(self.conv2(x))
-#         x = self.pool2(x)
-
-#         x = self.flatten(x)
-
-#         x = F.relu(self.fc1(x))
-#         x = self.fc2(x)
-#         return x
